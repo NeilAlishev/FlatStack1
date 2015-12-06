ead94236d0a37b0fbc5f1dfef4c7188b6022aa9c
Nail Alishev <nailalishev@yahoo.com>
2015-12-06 21:00:08 +0300

    Bugs fixed

--- Blog/app/assets/javascripts/comments.js
+++ Blog/app/assets/javascripts/comments.js
@@ -2,7 +2,6 @@ $(document).on('page:change', function() {
 	$('#show-comments').click(function(event) {
 		event.preventDefault();
 		$('.comments-section').slideToggle();
-		$('#comment_content').focus();
 		var text = $('#show-comments').text();
 		if (text == 'Показать комментарии') {
 			$('#show-comments').text('Скрыть комментарии');
